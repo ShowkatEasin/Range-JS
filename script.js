55f4e2133ef6539f6eb0de812ee7be1db19078a0
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-10-13 00:26:27 +0600

    Console

--- script.js
+++ script.js
@@ -1,65 +1,70 @@
-function add(){
-    var val=document.getElementById("val").value;
+// function add(){
+//     var val=document.getElementById("val").value;
 
-    val++;
-    document.getElementById("val").value=val;
-}
-function sub(){
-    var val=document.getElementById("val").value;
-    if(val>0){
-        val--;
-    }
-    else{
-        val=0 ;
-    }
-    document.getElementById("val").value=val;
-}
+//     val++;
+//     document.getElementById("val").value=val;
+// }
+// function sub(){
+//     var val=document.getElementById("val").value;
+//     if(val>0){
+//         val--;
+//     }
+//     else{
+//         val=0 ;
+//     }
+//     document.getElementById("val").value=val;
+// }
 
 
 
-function range(){
+// function range(){
 
-    var range = document.getElementById("range").value;
-    document.getElementById("val").value=range;
+//     var range = document.getElementById("range").value;
+//     document.getElementById("val").value=range;
 
-}
+// }
 
-const int = setInterval (test,100)
+// const int = setInterval (test,100)
 
-function test(){
+// function test(){
 
-    var s =document.getElementById("s").value ;
-    var m =document.getElementById("m").value ;
-    var h =document.getElementById("h").value ;
+//     var s =document.getElementById("s").value ;
+//     var m =document.getElementById("m").value ;
+//     var h =document.getElementById("h").value ;
   
-    s++;
-    var formate ="PM";
+//     s++;
+//     var formate ="PM";
 
-    if(s==10){
-        s=0;
-        m++;
-        if(m==10){
-            m=0;
-            h++;
-            if(h==24){
-                h=0;
-                formate ="AM";
-            }
+//     if(s==10){
+//         s=0;
+//         m++;
+//         if(m==10){
+//             m=0;
+//             h++;
+//             if(h==24){
+//                 h=0;
+//                 formate ="AM";
+//             }
            
-        }
+//         }
 
-    }
-
-
+//     }
 
 
 
-    document.getElementById("s").value=s;
-    document.getElementById("m").value=m;
-    document.getElementById("h").value=h;
-    document.getElementById("f").value=formate;
+//     document.getElementById("s").value=s;
+//     document.getElementById("m").value=m;
+//     document.getElementById("h").value=h;
+//     document.getElementById("f").value=formate;
 
+// }
+// function stop(){
+//     clearInterval (int);
+// }
+
+
+var sum=0;
+for (var a=1; a <=100; a++){
+    sum +=a;
 }
-function stop(){
-    clearInterval (int);
-}
+console.log(sum);
